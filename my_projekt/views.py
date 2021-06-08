1aebd0cf42ed2392c859cf4ee6ab566818e1c836
Ibrhim <Ibrhim.crysis@gmail.com>
2021-06-08 18:50:07 +0400

    git commit -m "adding files"

--- my_projekt/views.py
+++ my_projekt/views.py
@@ -84,20 +84,22 @@ def index2(request):
 
 
 def log_in(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['login']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-                response = redirect('/')
-                response.delete_cookie('cart_count')
-                return response
-            else:
-                form.add_error('login', 'Bad login or password')
-                form.add_error('password', 'Bad login or password')
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        username = form.cleaned_data['login']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            user_id = user.id
+            cart = Cart.objects.get(user_id=user_id)
+            cart_records = CartContent.objects.filter(cart_id=cart.id)
+            login(request, user)
+            response = redirect('/')
+            response.set_cookie('cart_count', len(cart_records))
+            return response
+        else:
+            form.add_error('login', 'Bad login or password')
+            form.add_error('password', 'Bad login or password')
     else:
         form = LoginForm()
 
@@ -128,31 +130,43 @@ class bay_a_car(MasterView):
             avatars = User_Profile.objects.get(user=request.user)
         else:
             avatars = '/static/img/no_avatar.png'
-        search_query = request.GET.get('search', '')
-        if search_query:
-            all_cars = Car.objects.filter(Q(color__icontains=search_query | Q(title__icontains=search_query)))
-        else:
-            all_cars = Car.objects.all()
         paginator = Paginator(self.all_cars, 6)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         form = SearchForm()
-        return render(request, 'bay_tesla_cars.html',
-                      {'cars': page_obj, 'form': form, 'user': request.user,  'avatars': avatars,
-                       'page_obj': page_obj})
+        if self.request.user.is_authenticated:
+            cart_content = self.get_cart_records()
+            response = render(request, 'bay_tesla_cars.html',
+                              {'cars': page_obj, 'form': form, 'user': request.user, 'avatars': avatars,
+                               'page_obj': page_obj})
+            response.set_cookie('cart_count', len(cart_content))
+            return response
+        else:
+            return render(request, 'bay_tesla_cars.html',
+                          {'cars': page_obj, 'form': form, 'user': request.user, 'avatars': avatars,
+                           'page_obj': page_obj})
 
     def post(self, request):
-        # avatars = User_Profile.objects.get(user=request.user)
         all_cars = Car.objects.all()
         form = SearchForm(request.POST)
         search = request.POST.get('search')
         if form.is_valid() and search:
-            all_cars = all_cars.filter(Q(title__contains=search) | Q(description__contains=search) | Q(color__contains=search) | Q(price__contains=search))
+            all_cars = all_cars.filter(
+                Q(title__contains=search) | Q(description__contains=search) | Q(color__contains=search) | Q(
+                    price__contains=search))
         else:
             form = SearchForm()
 
-        return render(request, 'bay_tesla_cars.html', {'cars': all_cars, 'form': form,  # 'avatars': avatars,
-                                                       'user': request.user})
+        if self.request.user.is_authenticated:
+            cart_content = self.get_cart_records()
+            response = render(request, 'bay_tesla_cars.html', {'cars': all_cars, 'form': form,
+                                                               'user': request.user})
+            response.set_cookie('cart_count', len(cart_content))
+            return response
+        else:
+            response = render(request, 'bay_tesla_cars.html', {'cars': all_cars, 'form': form,
+                                                               'user': request.user})
+            return response
 
 
 def user_avatar(request):
